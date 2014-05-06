99fd63b167420d31b1cd7f970267574d6f66c324
MacSteini <MacSteini@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-06 20:45:51 +0000

    Updated
    git-svn-id: https://plugins.svn.wordpress.org/wp-browser-update/trunk@909225 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- WP-BrowserUpdate.php
+++ WP-BrowserUpdate.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: WP BrowserUpdate
-Plugin URI: http://wordpress.org/extend/plugins/wp-browser-update
-Description: This plugin informs website visitors to update their out-dated browser in an unobtrusive way. Visit <a href="http://browser-update.org/" title="browser-update.org" target="_blank">browser-update.org</a> for more information...
-Version: 2.0.4
+Plugin URI: http://blog.steini.me/wp-browserupdate
+Description: This plugin informs website visitors to update their out-dated browser in an unobtrusive way. Go to <a href="http://browser-update.org/" title="browser-update.org" target="_blank">browser-update.org</a> for more information...
+Version: 2.1.0
 Author: Marco Steinbrecher
 Author URI: http://profiles.wordpress.org/macsteini
 Min WP Version: 1.5.1
@@ -13,6 +13,7 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
 if (!get_option('wp_browserupdate_browsers')) update_option('wp_browserupdate_browsers', '');
+if (!get_option('wp_browserupdate_js')) update_option('wp_browserupdate_js', '0 false true');
 if (!get_option('wp_browserupdate_css_buorg')) update_option('wp_browserupdate_css_buorg', '');
 if (!get_option('wp_browserupdate_css_buorgdiv')) update_option('wp_browserupdate_css_buorgdiv', '');
 if (!get_option('wp_browserupdate_css_buorga')) update_option('wp_browserupdate_css_buorga', '');
@@ -24,15 +25,30 @@ load_plugin_textdomain('WPBU_by_Steini', false, basename(dirname(__FILE__)).'/la
 }
 
 function wpbu() {
-$wpbu_vars = explode(' ', get_option('wp_browserupdate_browsers'));
+$browser = '';
 
+$wpbu_vars = explode(' ', get_option('wp_browserupdate_browsers'));
 $msie = $wpbu_vars[0];
 $firefox = $wpbu_vars[1];
 $opera = $wpbu_vars[2];
 $safari = $wpbu_vars[3];
 
+$wpbu_js = explode(' ', get_option('wp_browserupdate_js'));
+$reminder = !empty($wpbu_js[0]) ? (int)$wpbu_js[0] : '';
+$testing = !empty($wpbu_js[1]) ? $wpbu_js[1] : '';
+$newwindow = !empty($wpbu_js[2]) ? $wpbu_js[2] : '';
+
+if (!empty($reminder)) $reminder = 'reminder:'.$reminder.',';
+if (!empty($testing)) $testing = 'test:'.$testing.',';
+if (!empty($newwindow)) $newwindow = 'newwindow:'.$newwindow.',';
+
+if (!empty($msie)) $browser .= 'i:'.$msie.',';
+if (!empty($firefox)) $browser .= 'f:'.$firefox.',';
+if (!empty($opera)) $browser .= 'o:'.$opera.',';
+if (!empty($safari)) $browser .= 's:'.$safari;
+
 echo '<script type="text/javascript">
-var $buoop = {'.((empty($msie) and empty($firefox) and empty($opera) and empty($safari)) ? '' : 'vs:{i:'.$msie.',f:'.$firefox.',o:'.$opera.',s:'.$safari.',n:9}').'}
+var $buoop = {'.str_replace(',,', ',', 'vs:{'.$browser.'},'.$reminder.','.$testing.','.$newwindow).'};
 $buoop.ol = window.onload;
 window.onload=function(){
 try {if ($buoop.ol) $buoop.ol();}catch (e) {}
@@ -47,6 +63,7 @@ document.body.appendChild(e);
 function wpbu_administration() {
 if (isset($_POST['wpbu_submit'])) {
 update_option('wp_browserupdate_browsers', $_POST['wpbu_msie'].' '.$_POST['wpbu_firefox'].' '.$_POST['wpbu_opera'].' '.$_POST['wpbu_safari']);
+update_option('wp_browserupdate_js', (int)$_POST['wpbu_reminder'].' '.$_POST['wpbu_testing'].' '.$_POST['wpbu_newwindow']);
 update_option('wp_browserupdate_css_buorg', $_POST['wpbu_css_buorg']);
 update_option('wp_browserupdate_css_buorgdiv', $_POST['wpbu_css_buorgdiv']);
 update_option('wp_browserupdate_css_buorga', $_POST['wpbu_css_buorga']);
@@ -61,15 +78,20 @@ $firefox = $wpbu_vars[1];
 $opera = $wpbu_vars[2];
 $safari = $wpbu_vars[3];
 
+$wpbu_js = explode(' ', get_option('wp_browserupdate_js'));
+$wpbu_reminder = $wpbu_js[0];
+$wpbu_testing = $wpbu_js[1];
+$wpbu_newwindow = $wpbu_js[2];
+
 $wpbu_css_buorg = get_option('wp_browserupdate_css_buorg');
 $wpbu_css_buorgdiv = get_option('wp_browserupdate_css_buorgdiv');
 $wpbu_css_buorga = get_option('wp_browserupdate_css_buorga');
 $wpbu_css_buorgclose = get_option('wp_browserupdate_css_buorgclose');
 
-$msie_vers = array(11, 10, 9, 8, 7, 6);
-$firefox_vers = array(29, 25, 20, 15, 10, 6);
-$opera_vers = array(20, 19, 18, 17, 16, 15);
-$safari_vers = array(7, 6, 5, 4, 3);
+$msie_vers = array(10, 9, 8, 7, 6);
+$firefox_vers = array(25, 20, 15, 10);
+$opera_vers = array(19, 18, 17, 16, 15);
+$safari_vers = array(6, 5, 4, 3);
 
 echo '<div class="wrap"><form action="'.$_SERVER['REQUEST_URI'].'" method="post"><h2>WP BrowserUpdate</h2><h3>'.__('Out-dated Browser Versions', 'WPBU_by_Steini').'</h3><p>'.__('Please choose which browser version you consider to be out-dated (of course, this will include all versions below)... If you leave as is, WP BrowserUpdate uses the default values.', 'WPBU_by_Steini').'</p><p>Microsoft Internet Explorer: <select name="wpbu_msie">';
 
@@ -87,16 +109,7 @@ echo '</select> <a href="http://opera.com/" title="'.__('Download', 'WPBU_by_Ste
 
 for ($x=0; $x<count($safari_vers); $x++) echo '<option value="'.$safari_vers[$x].'"'.($safari==$safari_vers[$x] ? ' selected="selected"' : '').'>'.$safari_vers[$x].'</option>';
 
-echo '</select> <a href="http://apple.com/safari" title="'.__('Download', 'WPBU_by_Steini').'" target="_blank">'.__('Download', 'WPBU_by_Steini').'</a></p>
-
-<h3>'.__('CSS Styles', 'WPBU_by_Steini').'</h3>
-<p>'.__('If you do not know about CSS and how to use, leave the following forms empty. For further details, visit the <a href="http://browser-update.org/customize.html" target="_blank">how-to page</a>.', 'WPBU_by_Steini').'</p>
-<p>.buorg '.__('Style', 'WPBU_by_Steini').':<br /><textarea name="wpbu_css_buorg" cols="40" rows="10">'.$wpbu_css_buorg.'</textarea></p>
-<p>.buorg div '.__('Style', 'WPBU_by_Steini').':<br /><textarea name="wpbu_css_buorgdiv" cols="40" rows="2">'.$wpbu_css_buorgdiv.'</textarea></p>
-<p>.buorg a '.__('Style', 'WPBU_by_Steini').':<br /><textarea name="wpbu_css_buorga" cols="40" rows="2">'.$wpbu_css_buorga.'</textarea></p>
-<p>#buorgclose '.__('Style', 'WPBU_by_Steini').':<br /><textarea name="wpbu_css_buorgclose" cols="40" rows="10">'.$wpbu_css_buorgclose.'</textarea></p>
-
-<p class="submit"><input type="submit" name="wpbu_submit" value="'.__('Update Settings', 'WPBU_by_Steini').'" /></p></form></div>';
+echo '</select> <a href="http://apple.com/safari" title="'.__('Download', 'WPBU_by_Steini').'" target="_blank">'.__('Download', 'WPBU_by_Steini').'</a></p><h3>'.__('Script Customizations', 'WPBU_by_Steini').'</h3><p>'.__('After how many hours the message should re-appear (0 = Show all the time)', 'WPBU_by_Steini').':<br /><input type="number" value="'.$wpbu_reminder.'" name="wpbu_reminder" min="0" max="99" step="1" required placeholder="(min: 0, max: 99)" /></p><p>'.__('Open link on notification bar in a new browser window/tab', 'WPBU_by_Steini').':<br /><select name="wpbu_newwindow"><option value="true"'.($wpbu_newwindow=='true' ? ' selected' : '').'>'.__('Yes', 'WPBU_by_Steini').'</option><option value="false"'.($wpbu_newwindow=='false' ? ' selected' : '').'>'.__('No', 'WPBU_by_Steini').'</option></select></p><p>'.__('Always show notification bar (for testing purposes)', 'WPBU_by_Steini').':<br /><select name="wpbu_testing"><option value="true"'.($wpbu_testing=='true' ? ' selected' : '').'>'.__('Yes', 'WPBU_by_Steini').'</option><option value="false"'.($wpbu_testing=='false' ? ' selected' : '').'>'.__('No', 'WPBU_by_Steini').'</option></select></p><h3>'.__('CSS Styles', 'WPBU_by_Steini').'</h3><p>'.sprintf(__('If you do not know about CSS and how to use, leave the following forms empty. For further details, visit the %show-to page%s.', 'WPBU_by_Steini'), '<a href="http://browser-update.org/customize.html" target="_blank">', '</a>').'</p><p>.buorg '.__('Style', 'WPBU_by_Steini').':<br /><textarea name="wpbu_css_buorg" cols="40" rows="10">'.$wpbu_css_buorg.'</textarea></p><p>.buorg div '.__('Style', 'WPBU_by_Steini').':<br /><textarea name="wpbu_css_buorgdiv" cols="40" rows="2">'.$wpbu_css_buorgdiv.'</textarea></p><p>.buorg a '.__('Style', 'WPBU_by_Steini').':<br /><textarea name="wpbu_css_buorga" cols="40" rows="2">'.$wpbu_css_buorga.'</textarea></p><p>#buorgclose '.__('Style', 'WPBU_by_Steini').':<br /><textarea name="wpbu_css_buorgclose" cols="40" rows="10">'.$wpbu_css_buorgclose.'</textarea></p><p class="submit"><input type="submit" name="wpbu_submit" value="'.__('Update Settings', 'WPBU_by_Steini').'" /></p></form></div>';
 }
 
 function wpbu_css() {
--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Tags: Browser, Update, Installation, Notice, Warning, Out-dated
 Requires at least: 1.5.1
 Tested up to: 3.9
 Compatible up to: 3.9
-Stable tag: 2.0.4
+Stable tag: 2.1.0
 License: GPLv2 or later
 License URI: http://gnu.org/licenses/gpl-2.0.html
 
@@ -14,9 +14,9 @@ This plugin informs website visitors to update their out-dated browser in an uno
 == Description ==
 Many internet users are surfing with out-dated browsers for several reasons (mainly because they do not know how to update). Switching to a newer browser is better in terms of security and reliability. This plugin informs your visitors to switch to a newer browser in an unobtrusive way. Just activate this plugin and you and your visitors are good to go...
 
-Visit <a href="http://browser-update.org/" title="browser-update.org" target="_blank">browser-update.org</a> for more information.
+Go to <a href="http://browser-update.org/" title="browser-update.org" target="_blank">browser-update.org</a> for more information.
 
-If you wish to contribute your own language, please have a look into the plugin's language folder. There you'll find WPBU_by_Steini.pot which can be edited with <a href="http://poedit.net/" title="Poedit" target="_blank">Poedit</a>. Send your translation <a href="mailto:wp-browserupdate@REMOVE-THISsteini.me">to me</a> afterwards. I appreciate your support!
+If you wish to contribute your own language, please have a look into the plugin's language folder. There you'll find WPBU_by_Steini.po which can be edited with <a href="http://poedit.net/" title="Poedit" target="_blank">Poedit</a>. Alternatively, feel free to drop me a plain-text e-mail with the translated texts and I'll compile afterwards. Send your translation <a href="mailto:wp-browserupdate@REMOVE-THISsteini.me">to me</a> afterwards. I really appreciate your support!
 
 == Installation ==
 1. Unzip file.
@@ -24,23 +24,26 @@ If you wish to contribute your own language, please have a look into the plugin'
 3. Activate the plugin through the plugins menu in WordPress backend
 
 == Changelog ==
+= 2.1.0 =
+* Added options to customize the JavaScript (big thanks to Tomas Risberg)
+
 = 2.0.4 =
 * Updated (out-dated) Browser Versions
-* Updated JavaScript (big thanks to Tomas Risberg)
+* Updated JavaScript
 
 = 2.0.3 =
-* French translation added (big thanks to Thierry Fosse)
+* Added French translation (big thanks to Thierry Fosse)
 
 = 2.0.2 =
-* Settings link to plugin overview page added
+* Added settings link to plugin overview page
 
 = 2.0.1 =
 * Minor bug fix
 
 = 2.0 =
-* English and German translations added
-* Administration panel for individual modifications added
-* Uninstall function added
+* Added English and German translations
+* Added Administration panel for individual modifications
+* Added uninstall function
 
 = 1.0 =
 * Stable Version
