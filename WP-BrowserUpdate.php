132511fc3fd91083b4af975e6bc2521214540e86
MacSteini <MacSteini@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-04 15:42:15 +0000

    Updated
    git-svn-id: https://plugins.svn.wordpress.org/wp-browser-update/trunk@979690 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- WP-BrowserUpdate.php
+++ WP-BrowserUpdate.php
@@ -3,7 +3,7 @@
 Plugin Name: WP BrowserUpdate
 Plugin URI: http://blog.steini.me/wp-browserupdate
 Description: This plugin informs website visitors to update their out-dated browser in an unobtrusive way. Go to <a href="http://browser-update.org/" title="browser-update.org" target="_blank">browser-update.org</a> for more information...
-Version: 2.1.2
+Version: 2.1.3
 Author: Marco Steinbrecher
 Author URI: http://profiles.wordpress.org/macsteini
 Min WP Version: 1.5.1
